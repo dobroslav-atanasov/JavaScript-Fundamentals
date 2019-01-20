5e8d02a79849ce34f2fd4d828ac46adb6329e409
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-01-20 18:59:47 +0200

    Updated task - Register Notification.

--- 04.DocumentObjectModel-Lab/04.Register-Notification/solution.js
+++ 04.DocumentObjectModel-Lab/04.Register-Notification/solution.js
@@ -1,4 +1,25 @@
+function register() {
+  let username = document.getElementById('username').value;
+  let email = document.getElementById('email').value;
+  let password = document.getElementById('password').value;
 
- function register() {
-   //TODO
- }
+  let pattern = /(.+)@(.+).(com|bg)/gm;
+  let validEmail = pattern.exec(email);
+
+  if (username && password && validEmail) {
+    setTimeout(() => {
+      let registrationElement = document.createElement('h1');
+      registrationElement.textContent = 'Successful Registration!';
+  
+      let newLine = document.createElement('br');
+      let secondNewLine = document.createElement('br');
+      let sectionElement = document.getElementById('result');
+      sectionElement.appendChild(registrationElement);
+      sectionElement.append(`Username: ${username}`);
+      sectionElement.appendChild(newLine);
+      sectionElement.append(`Email: ${email}`);
+      sectionElement.appendChild(secondNewLine);
+      sectionElement.append(`Password: ${'*'.repeat(password.length)}`);
+    }, 5000);
+  }
+}

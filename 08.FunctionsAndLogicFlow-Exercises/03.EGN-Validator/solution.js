5a93f18cd323db0c6aa548393d09f8c6cd2dfe30
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-01-24 22:09:07 +0200

    Uploaded solution of task 3, 4 and 5.

--- 08.FunctionsAndLogicFlow-Exercises/03.EGN-Validator/solution.js
+++ 08.FunctionsAndLogicFlow-Exercises/03.EGN-Validator/solution.js
@@ -2,35 +2,52 @@ function validate() {
     let yearElement = document.getElementById('year');
     let monthElement = document.getElementById('month');
     let dateElement = document.getElementById('date');
-
+    let maleElement = document.getElementById('male');
+    let femaleElement = document.getElementById('female');
+    let codeElement = document.getElementById('region');
     let button = document.querySelector('button');
 
     button.addEventListener('click', getEgn);
-    function getEgn(e) {
-        let egnString = '';
-        egnString += yearElement.value;
-        egnString += getMonthNumber();
-        egnString += dateElement.value;
+    function getEgn() {
+        let egn = '';
+        let year = yearElement.value;
+        let month = monthElement.selectedIndex;
+        let day = dateElement.value;
+        let gender = maleElement.checked ? 2 : 1;
+        let regionCode = codeElement.value;
 
-        console.log(egnString);
+        if (year >= 1900 && year <= 2100 &&  regionCode >= 43 && regionCode <= 999) {
+            egn = `${year.substr(-2)}${addZeroToNumber(month)}${addZeroToNumber(day)}${regionCode.slice(0, 2)}${gender}`;
+            let controlDigit = getControlDigit(egn);
+
+            document.getElementById('egn').textContent = `Your EGN is: ${egn}${controlDigit}`;
+
+            yearElement.value = '';
+            monthElement.value = '';
+            dateElement.value = '';
+            maleElement.checked = false;
+            femaleElement.checked = false;
+            codeElement.value = '';
+        }
     }
 
-    function getMonthNumber() {
-        switch (monthElement.value) {
-            case 'January': return '01';
-            case 'February': return '02';
-            case 'March': return '03';
-            case 'April': return '04';
-            case 'May': return '05';
-            case 'June': return '06';
-            case 'July': return '07';
-            case 'August': return '08';
-            case 'September': return '09';
-            case 'October': return '10';
-            case 'November': return '11';
-            case 'December': return '12';
-            default:
-                break;
+    function getControlDigit(egn) {
+        let weightPosition = [2, 4, 8, 5, 10, 9, 7, 3, 6];
+        let sum = 0;
+
+        for (let i = 0; i < egn.length; i++) {
+            sum += Number(egn[i] * weightPosition[i]);
         }
+
+        let controlDigit = sum % 11 <= 9 ? sum % 11 : 0;
+        return controlDigit;
+    }
+
+    function addZeroToNumber(number) {
+        if (Number(number) < 10) {
+            return `0${number}`;
+        }
+
+        return number;
     }
 }

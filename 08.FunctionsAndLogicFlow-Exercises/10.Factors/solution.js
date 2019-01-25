d82fe57235192205ce44e0c4a2426f5510c80d39
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2019-01-25 16:21:56 +0200

    Uploaded solution of task 10.

--- 08.FunctionsAndLogicFlow-Exercises/10.Factors/solution.js
+++ 08.FunctionsAndLogicFlow-Exercises/10.Factors/solution.js
@@ -1,3 +1,12 @@
 function solve() {
-   // TODO ...
+   let number = Number(document.getElementById('num').value);
+   let result = [1];
+
+   for (let i = 2; i <= number; i++) {
+      if (number % i === 0) {
+         result.push(i);
+      }
+   }
+
+   document.getElementById('result').textContent = result.join(' ');
 }

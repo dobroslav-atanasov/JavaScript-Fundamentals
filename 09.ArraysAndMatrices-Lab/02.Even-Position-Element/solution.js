590dc9848287f47ad2a3319edf0df8412c707884
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-01-26 12:31:26 +0200

    Uploaded solutions of the lab - Arrays and Matrices.

--- 09.ArraysAndMatrices-Lab/02.Even-Position-Element/solution.js
+++ 09.ArraysAndMatrices-Lab/02.Even-Position-Element/solution.js
@@ -1,3 +1,12 @@
 function solve() {
-  // TODO
+  let array = JSON.parse(document.getElementById('arr').value).map(x => parseInt(x, 10));
+  let result = [];
+
+  for (let index in array) {
+    if (index % 2 === 0) {
+      result.push(array[index]);
+    }
+  }
+
+  document.getElementById('result').textContent = result.join(' x ');
 }

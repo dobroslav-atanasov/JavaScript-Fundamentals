96337dc06b7e52c03122bde4b3fbce36bc3b50d3
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-01-27 13:07:45 +0200

    Uploaded solution of task Order the Names.

--- 06.DocumentObjectModel-MoreExercises/02.Order-the-Names/solution.js
+++ 06.DocumentObjectModel-MoreExercises/02.Order-the-Names/solution.js
@@ -1,3 +1,32 @@
-function solve(){
-    //TODO
+function solve() {
+    let buttonElement = document.querySelector('button');
+    buttonElement.addEventListener('click', clickEvent);
+    let letters = getLetters();
+
+    function clickEvent() {
+        let name = document.querySelector('input').value;
+        let firstLetter = name[0].toLowerCase();
+        let index = letters.indexOf(firstLetter);
+
+        let liElements = document.querySelectorAll('li');
+        let li = liElements[index];
+        let names = [];
+        if (li.textContent.length > 0) {
+            names = li.textContent.split(', ');
+        }
+        names.push(name);
+        li.textContent = names.join(', ');
+
+        document.querySelector('input').value = '';
+    }
+
+    function getLetters() {
+        let letters = [];
+
+        for (let i = 97; i <= 122; i++) {
+            letters.push(String.fromCharCode(i));
+        }
+
+        return letters;
+    }
 }

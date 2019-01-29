d631d84e967395fa5f53b67cff20e8fa723b08e9
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-01-29 20:15:24 +0200

    Modified solution of Order the names.

--- 06.DocumentObjectModel-MoreExercises/02.Order-the-Names/solution.js
+++ 06.DocumentObjectModel-MoreExercises/02.Order-the-Names/solution.js
@@ -1,32 +1,21 @@
 function solve() {
     let buttonElement = document.querySelector('button');
     buttonElement.addEventListener('click', clickEvent);
-    let letters = getLetters();
 
     function clickEvent() {
-        let name = document.querySelector('input').value;
-        let firstLetter = name[0].toLowerCase();
-        let index = letters.indexOf(firstLetter);
-
-        let liElements = document.querySelectorAll('li');
-        let li = liElements[index];
-        let names = [];
-        if (li.textContent.length > 0) {
-            names = li.textContent.split(', ');
+        let input = document.querySelector('input');
+        let firstLetterIndex = input.value.toLowerCase().charCodeAt(0) - 97;
+        if (firstLetterIndex >= 0 && firstLetterIndex <= 25) {
+            name = input.value.charAt(0).toUpperCase() + input.value.substr(1).toLowerCase();
+            let liElements = document.querySelectorAll('li');
+            let li = liElements[firstLetterIndex];
+            if (li.textContent === "") {
+                li.textContent += name;
+            } else {
+                li.textContent += ", " + name;
+            }
+    
+            document.querySelector('input').value = '';
         }
-        names.push(name);
-        li.textContent = names.join(', ');
-
-        document.querySelector('input').value = '';
-    }
-
-    function getLetters() {
-        let letters = [];
-
-        for (let i = 97; i <= 122; i++) {
-            letters.push(String.fromCharCode(i));
-        }
-
-        return letters;
     }
 }

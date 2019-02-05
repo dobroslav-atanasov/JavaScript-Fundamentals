95f2a1e66c9477abe09a4dc5e6ad7b579dec85c8
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2019-02-05 13:49:50 +0200

    Uploaded part of solution of Forum task.

--- 15.ObejctsAndJSON-Exercises/03.Forum/solution.js
+++ 15.ObejctsAndJSON-Exercises/03.Forum/solution.js
@@ -1,3 +1,33 @@
 function solve() {
- // TODO
+    let users = [];
+    let submitButton = document.getElementsByTagName('button')[0];
+    submitButton.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        let userDiv = document.querySelectorAll('div.user-info input');
+        let name = userDiv[0].value;
+        let password = userDiv[1].value;
+        let email = userDiv[2].value;
+        users.push({name, password, email});
+
+        let tdUsername = document.createElement('td');
+        tdUsername.textContent = name;
+        let tdEmail = document.createElement('td');
+        tdEmail.textContent = email;
+        let tdPassword = document.createElement('td');
+        tdPassword.textContent = password;
+
+        let tr = document.createElement('tr');
+        tr.appendChild(tdUsername);
+        tr.appendChild(tdEmail);
+        tr.appendChild(tdPassword);
+
+        let tableElement = document.getElementsByTagName('tbody')[0];
+        tableElement.appendChild(tr);
+    });
+
+    let searchButton = document.getElementsByTagName('button')[1];
+    searchButton.addEventListener('click', (e) => {
+        e.preventDefault();
+    });
 }

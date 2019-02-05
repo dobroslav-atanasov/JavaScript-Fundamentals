efe692db042b930857741e4242668109e7e060fd
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-02-05 19:34:21 +0200

    Modified solution of task Forum.

--- 15.ObejctsAndJSON-Exercises/03.Forum/solution.js
+++ 15.ObejctsAndJSON-Exercises/03.Forum/solution.js
@@ -1,5 +1,4 @@
 function solve() {
-    let users = [];
     let submitButton = document.getElementsByTagName('button')[0];
     submitButton.addEventListener('click', (e) => {
         e.preventDefault();
@@ -8,26 +7,49 @@ function solve() {
         let name = userDiv[0].value;
         let password = userDiv[1].value;
         let email = userDiv[2].value;
-        users.push({name, password, email});
+
+        let topics = [];
+        let topicsDiv = document.querySelectorAll('div.topics input');
+        for (let topic of topicsDiv) {
+            if (topic.checked) {
+                topics.push(topic.value);
+            }
+        }
 
         let tdUsername = document.createElement('td');
         tdUsername.textContent = name;
         let tdEmail = document.createElement('td');
         tdEmail.textContent = email;
-        let tdPassword = document.createElement('td');
-        tdPassword.textContent = password;
+        let tdTopics = document.createElement('td');
+        tdTopics.textContent = topics.join(' ');
 
         let tr = document.createElement('tr');
         tr.appendChild(tdUsername);
         tr.appendChild(tdEmail);
-        tr.appendChild(tdPassword);
+        tr.appendChild(tdTopics);
 
         let tableElement = document.getElementsByTagName('tbody')[0];
         tableElement.appendChild(tr);
+
+        userDiv[0].value = '';
+        userDiv[1].value = '';
+        userDiv[2].value = '';
     });
 
     let searchButton = document.getElementsByTagName('button')[1];
     searchButton.addEventListener('click', (e) => {
-        e.preventDefault();
+        let searchedText = document.querySelector('div input[placeholder="Search..."]').value;
+        let users = document.querySelectorAll('tbody tr');
+        for (let user of users) {
+            let name = user.children[0].textContent;
+            let email = user.children[1].textContent;
+            let topics = user.children[2].textContent;
+
+            if (name.includes(searchedText) || email.includes(searchedText) || topics.includes(searchedText)) {
+                user.style.visibility = 'visible';
+            } else {
+                user.style.visibility = 'hidden';
+            }
+        }
     });
 }

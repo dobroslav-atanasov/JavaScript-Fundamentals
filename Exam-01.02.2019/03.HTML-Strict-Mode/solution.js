3029d78860729a423463fced194971dc47304820
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-02-10 19:26:57 +0200

    Updated solution to 100/100.

--- Exam-01.02.2019/03.HTML-Strict-Mode/solution.js
+++ Exam-01.02.2019/03.HTML-Strict-Mode/solution.js
@@ -1,12 +1,18 @@
 function solve(array) {
     let result = [];
-    let pattern = /<(\/?\w+)((\s+\w+(\s*=\s*(?:".*?"|'.*?'|[\^'">\s]+))?)+\s*|\s*)\/?>/g;
-
+    let validArray = [];
+    let tagPattern = /^<[^<*\s>](.*)>$/g;
     for (let i = 0; i < array.length; i++) {
+        while (match = tagPattern.exec(array[i])) {
+            validArray.push(match[0]);
+        }
+    }
+
+    let pattern = /<(\/?\w+)((\s+\w+(\s*=\s*(?:".*?"|'.*?'|[\^'">\s]+))?)+\s*|\s*)\/?>/g;
+    for (let i = 0; i < validArray.length; i++) {
         let tags = [];
-        while (match = pattern.exec(array[i])) {
-            let asd = match[1];
-            tags.push(asd);
+        while (match = pattern.exec(validArray[i])) {
+            tags.push(match[1]);
         }
 
         if (tags.length % 2 !== 0) {
@@ -25,8 +31,8 @@ function solve(array) {
             continue;
         }
 
-        let wordPattern = />(.*?)</g;
-        while (match = wordPattern.exec(array[i])) {
+        let wordPattern = /[^*\s<]>(.*?)</g;
+        while (match = wordPattern.exec(validArray[i])) {
             if (match[1] !== '') {
                 result.push(match[1].trim());
             }
@@ -38,3 +44,4 @@ function solve(array) {
 
 solve(['<h1><span>Hello World!</span></h1>', '<p>I am Peter.']);
 solve(['<div><p>This</p> is</div>', '<div><a>perfectly</a></div>', '<divs><p>valid</p></divs>', '<div><p>This</div></p>', '<div><p>is not</p><div>']);
+solve(['<*>F</*>', '<>F</>', '<<a>F</a>', '<a>F</a>']);
